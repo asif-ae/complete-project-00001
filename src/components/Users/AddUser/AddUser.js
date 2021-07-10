fa0e1ee6cb3c7f5835376de818fc46cf39ecae38
Asif Estiak <me.asif.ae@gmail.com>
2021-07-10 08:28:41 +0600

    used the use ref hook instead of use state

--- src/components/Users/AddUser/AddUser.js
+++ src/components/Users/AddUser/AddUser.js
@@ -1,36 +1,42 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useRef, useState } from 'react';
 import Button from '../../UI/Button/Button';
 import Card from '../../UI/Card/Card';
 import ErrorModal from '../../UI/ErrorModal/ErrorModal';
 import { input, label } from './AddUser.module.css';
 
 const AddUser = ({ onAddUser }) => {
-  const [enteredData, setEnteredData] = useState({
-    enteredUsername: '',
-    enteredAge: '',
-  });
-  const [error, setError] = useState();
-  console.log(enteredData);
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+  
 
-  const onChangeHandler = (event) => {
-    console.log(event.target.value, event.target.name);
-    event.target.name === 'username' &&
-    setEnteredData( (prev) => { return { ...prev, enteredUsername: event.target.value } } );
-    event.target.name === 'age' &&
-    setEnteredData( (prev) => { return { ...prev, enteredAge: event.target.value } } );
-  }
+  // const [enteredData, setEnteredData] = useState({
+  //   enteredUsername: '',
+  //   enteredAge: '',
+  // });
+  const [error, setError] = useState();
+  // console.log(enteredData);
+
+  // const onChangeHandler = (event) => {
+  //   console.log(event.target.value, event.target.name);
+  //   event.target.name === 'username' &&
+  //   setEnteredData( (prev) => { return { ...prev, enteredUsername: event.target.value } } );
+  //   event.target.name === 'age' &&
+  //   setEnteredData( (prev) => { return { ...prev, enteredAge: event.target.value } } );
+  // }
 
   const addUserHandler = ( event ) => {
     event.preventDefault();
+    const enteredName = nameInputRef.current.value;
+    const enteredAge = ageInputRef.current.value;
 
-    if (enteredData.enteredUsername.trim().length === 0 || enteredData.enteredAge.trim().length === 0) {
+    if (enteredName.trim().length === 0 || enteredAge.trim().length === 0) {
       setError({
         title: 'Invalid input!',
         message: 'Please enter a valid name and age (non-empty values).',
       })
       return;
     }
-    if (+enteredData.enteredAge < 1) {
+    if (+enteredAge < 1) {
       setError({
         title: 'Invalid age!',
         message: 'Please enter a valid age (greater than > 0).',
@@ -39,8 +45,12 @@ const AddUser = ({ onAddUser }) => {
     }
 
     console.log(event.target.value);
-    onAddUser(enteredData.enteredUsername, enteredData.enteredAge)
-    setEnteredData( (prev) => { return { ...prev, enteredUsername: '', enteredAge: '' } } )
+    onAddUser(enteredName, enteredAge);
+
+    // Reset all input data
+    nameInputRef.current.value = '';
+    ageInputRef.current.value = '';
+    // setEnteredData( (prev) => { return { ...prev, enteredUsername: '', enteredAge: '' } } )
   }
 
   const errorHandler = () => {
@@ -59,10 +69,12 @@ const AddUser = ({ onAddUser }) => {
       <Card className={input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username" className={label}>Username</label>
-          <input type="text" name="username" id="username" className={input} onChange={onChangeHandler} value={enteredData.enteredUsername} />
+          {/* onChange={onChangeHandler} value={enteredData.enteredUsername} */}
+          <input type="text" name="username" id="username" className={input} ref={nameInputRef} />
 
           <label htmlFor="age">Age (Years)</label>
-          <input type="number" name="age" id="age" className={input} onChange={onChangeHandler} value={enteredData.enteredAge} />
+          {/* onChange={onChangeHandler} value={enteredData.enteredAge} */}
+          <input type="number" name="age" id="age" className={input} ref={ageInputRef} />
 
           <Button type="submit">Add User</Button>
         </form>
